3b5a7e1d07f22d227a60040839b671059038dc24
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2022-11-08 00:55:15 +0300

    hw3 added

---
+++ hw3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine()!);
+PrintCubes(Number);
+
+void PrintCubes(int Number)
+{
+    for (int i = 1; i <= Number; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+    }
+}
